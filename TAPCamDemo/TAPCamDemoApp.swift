b99930827865fa3fafc02e67ebb7ec0d8b043a6c
Harold <20009626+HaroldGin931@users.noreply.github.com>
2026-04-24 13:07:30 +0800

    Initial Commit

---
+++ TAPCamDemo/TAPCamDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TAPCamDemoApp.swift
+//  TAPCamDemo
+//
+//  Created by Harold on 2026/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TAPCamDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TAPCamDemoTests/TAPCamDemoTests.swift
@@ -0,0 +1,19 @@
+//
+//  TAPCamDemoTests.swift
+//  TAPCamDemoTests
+//
+//  Created by Harold on 2026/4/24.
+//
+
+import Testing
+@testable import TAPCamDemo
+
+struct TAPCamDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
